c6eaddf9852ef2bcd4bcbd97f4abbf272aa2e1c4
takahiro aikou <ubuntu@ip-172-31-43-239.ap-northeast-1.compute.internal>
2019-02-10 16:42:10 +0000

    initail commit

---
+++ ubuntu18.04/setup_myfavorites.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#########################
+#  target : Ubuntu18.04 #
+#########################
+
+
+# google chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+sudo apt update
+
+## Install
+sudo apt install google-chrome-stable
