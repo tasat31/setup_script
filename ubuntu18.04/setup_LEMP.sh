e099eadd2f2d3183f89c20b5e114c6ec463141d5
takahiro aikou <ubuntu@ip-172-31-43-239.ap-northeast-1.compute.internal>
2019-02-12 07:26:48 +0000

    initial commit

---
+++ ubuntu18.04/setup_LEMP.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#########################
+#  target : Ubuntu18.04 #
+#########################
+
+# https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-ubuntu-18-04
+
+sudo apt update
+
+sudo apt install nginx
+
+sudo apt install mysql-server
+sudo mysql_secure_installation
+
+
+# https://thishosting.rocks/install-php-on-ubuntu/
+
+sudo apt install php php-mysql
+
+echo "*** recommend Nginx configuration ***"
+echo "sudo ufw allow 'Nginx HTTP'"
+echo "sudo ufw status"
+echo
+echo "*** MySQL setup for your applications ***"
+echo "mysql -u root -p"
+
+# ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
