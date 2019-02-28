17d8b2f0b5a26b1665af6c7d0d61964a6967f92d
takahiro aikou <ta-aikou@heart-musen.com>
2019-02-28 10:55:29 +0900

    add install yarn

--- ubuntu18.04/setup_nodejs.sh
+++ ubuntu18.04/setup_nodejs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 # https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-18-04
 sudo apt update
 
@@ -8,6 +12,8 @@ sudo apt -y install npm
 
 sudo npm update -g npm
 sudo npm install -g n
+
+sudo apt install yarn
 
 echo ""
 echo ""
