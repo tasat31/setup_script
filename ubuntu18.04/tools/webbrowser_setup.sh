0b6a8781edb93dc6d62f52172cd512a12f801105
Ubuntu <ubuntu@ip-172-31-42-121.ap-northeast-1.compute.internal>
2019-05-12 23:13:10 +0000

    amend installing vivaldi

--- ubuntu18.04/tools/webbrowser_setup.sh
+++ ubuntu18.04/tools/webbrowser_setup.sh
@@ -13,15 +13,17 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
 sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
 
-sudo apt update
-
-## google chrome install
-sudo apt -y install google-chrome-stable
-## Opera install
-sudo apt -y install opera-stable
-
 # vivaldi
 wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
 sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
 
-sudo apt install vivaldi-stable
+sudo apt update
+
+## google chrome install
+sudo apt -y install google-chrome-stable
+
+## Opera install
+sudo apt -y install opera-stable
+
+# vivaldi install
+sudo apt -y install vivaldi-stable
